8aaf02b54fdb89213038b8ba9ab60eb5291a59f4
Matt Meshulam <matt.meshulam@tempo-db.com>
2015-02-02 12:04:11 -0600

    Rewrite import script to be parallel and use Tempo client

--- import.py
+++ import.py
@@ -1,3 +1,12 @@
+import gevent.monkey
+gevent.monkey.patch_all()  # Do this before other imports
+
+import gevent
+from gevent.queue import JoinableQueue
+
+from tempoiq.protocol.encoder import WriteEncoder, CreateEncoder
+from tempoiq.session import get_session
+import tempoiq.response
 import re
 import sys
 import os
@@ -9,62 +18,84 @@ DEVICE_FILE = r"devices\.json"
 DATA_FILE = r"datapoints[0-9]*\.tsv"
 DATA_PATH = '.'
 
+class Importer(object):
+    def __init__(self, creds, pool_size=5):
+        self.client = get_session(creds['host'],
+                                  creds['key'],
+                                  creds['secret'])
+        self.queue = JoinableQueue()
+        for i in range(pool_size):
+            gevent.spawn(self.worker)
 
-def delete_everything(creds):
-    delete_body = """{"search": {"select": "devices", "filters": {"""
-    delete_body += """"devices": "all"}},"find": {"quantifier":"all"}}"""
+    def worker(self):
+        while True:
+            job = self.queue.get()
+            typ = job.get('type')
+            try:
+                if typ == 'device':
+                    self._process_device(job['data'])
+                elif typ == 'datapoints':
+                    self._process_datapoints(job['data'])
+            finally:
+                self.queue.task_done()
 
-    device_url = creds['host'] + '/v2/devices'
-
-    res = requests.delete(device_url, data=delete_body,
-                          auth=(creds['key'], creds['secret']))
-    if (res.status_code != 200):
-        print("Delete failed: {} - {}".format(res.status_code, res.text))
-        sys.exit(1)
-
-
-def load_devices_from_file(filename, creds):
-    device_url = creds['host'] + '/v2/devices'
-
-    with open(filename) as device_file:
-        devices = json.load(device_file)
+    def write_devices(self, devices):
         for device in devices:
-            res = requests.post(device_url, data=json.dumps(device),
-                                auth=(creds['key'], creds['secret']))
-            if (res.status_code != 200):
-                if 'A device with that key already exists' in res.text:
-                    print("Skipping creating existing device {}"
-                          .format(device['key']))
-                else:
-                    print("Error creating device {}: code {}"
-                          .format(device['key'], res.status_code))
-                    sys.exit(1)
+            self.queue.put({'type': 'device', 'data': device})
+        self.queue.join()
 
-def write_points(points, creds):
-    data_url = creds['host'] + '/v2/write/'
-    res = requests.post(data_url, data=json.dumps(points),
-                        auth=(creds['key'], creds['secret']))
-
-    if (res.status_code != 200):
-        print("Error writing data! code {}"
-              .format(res.status_code))
-        sys.exit(1)
-
-def load_datapoints_from_file(filename, creds):
-    points = {}
-    lineno = 0
-    with open(filename) as point_file:
-        for line in point_file:
+    def write_datapoints_from_file(self, infile):
+        points = {}
+        lineno = 0
+        for line in infile:
             lineno += 1
             (device, sensor, ts, val) = line.split('\t')
             pts = points.setdefault(device, {}).setdefault(sensor, [])
             pts.append({'t': ts, 'v': float(val)})
 
             if lineno % 1000 == 0:
-                write_points(points, creds)
+                self.queue.put({'type': 'datapoints', 'data': points})
                 points = {}
 
-def main(argv):
+        if points:
+            self.queue.put({'type': 'datapoints', 'data': points})
+        self.queue.join()
+
+    def _process_device(self, device, retries=5):
+        res = self.client.create_device(device)
+        if res.successful != tempoiq.response.SUCCESS:
+            if 'A device with that key already exists' in res.body:
+                print("Skipping creating existing device {}"
+                      .format(device['key']))
+                return
+
+            if retries > 0:
+                print("Retrying device create {}, error {}"
+                      .format(device['key'], res.body))
+                self._process_device(device, retries - 1)
+            else:
+                print("Retries exceeded; couldn't create device {}"
+                      .format(device['key']))
+
+    def _process_datapoints(self, write_request, retries=5):
+        try:
+            res = self.client.write(write_request)
+        except Exception, e:
+            print("ERROR with request: --->")
+            print(json.dumps(write_request, default=WriteEncoder().default))
+            raise e
+
+        if res.successful != tempoiq.response.SUCCESS:
+            if retries > 0:
+                print("Retrying write, error was: {}".format(res.body))
+                return self._process_datapoints(write_request, retries - 1)
+            else:
+                print("Retries exceeded; lost data!")
+                print(json.dumps(write_request, default=WriteEncoder().default))
+                return True
+        return False
+
+def parse_credentials(argv):
     creds = {}
     try:
         opts, args = getopt.getopt(argv, "n:k:s:")
@@ -79,19 +110,38 @@ def main(argv):
         elif opt == "-n":
             creds['host'] = arg
 
-    if not creds['host'].startswith('http'):
-        creds['host'] = 'https://' + creds['host']
+    if 'key' in creds and 'secret' in creds and 'host' in creds:
+        if not creds['host'].startswith('http'):
+            creds['host'] = 'https://' + creds['host']
+        return creds
+    else:
+        return None
+
+def main(argv):
+    creds = parse_credentials(argv)
+    if not creds:
+        print("Invalid credentials!")
+        sys.exit(2)
+
+    importer = Importer(creds)
 
     files = os.listdir(DATA_PATH)
+    device_files = []
     data_files = []
     for f in files:
         if re.match(DATA_FILE, f):
             data_files.append(os.path.join(DATA_PATH, f))
         if re.match(DEVICE_FILE, f):
-            load_devices_from_file(os.path.join(DATA_PATH, f), creds)
+            device_files.append(os.path.join(DATA_PATH, f))
 
-    for data_file in data_files:
-        load_datapoints_from_file(data_file, creds)
+    for filename in device_files:
+        with open(filename) as device_file:
+            devices = json.load(device_file)
+            importer.write_devices(devices)
+
+    for filename in data_files:
+        with open(filename) as point_file:
+            importer.write_datapoints_from_file(point_file)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
